9a6a185023de1f6c9a8961202654fac2015310d1
rdborisov99 <rdborisov99@gmail.com>
2025-09-15 17:24:45 +0300

    add compose and redis

--- docker-ed/main.py
+++ docker-ed/main.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from redis import Redis
 import socket
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
 
 @app.route("/")
 def hello_world():
-    return f"<p>Hello, World!</p> {socket.gethostname()}"
+    count = redis.incr('hits')
+    return f"<p>Hello, World!</p> {socket.gethostname()} <p> seen: {count}"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8000", debug=True)
